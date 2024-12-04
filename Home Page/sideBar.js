0cc9b378551b1ae26d5fb9c331c93d7d0e1c3b3d
GojmirAjdini <gojmirajdinii@gmail.com>
2024-12-04 13:17:22 +0100

    commit

---
+++ Home Page/sideBar.js
@@ -0,0 +1,15 @@
+function showSideBar(){
+
+    const sidebar = document.querySelector('.sideBar')
+
+    sidebar.style.display = 'flex';
+
+}
+
+function closeSideBar(){
+
+    const sideBar = document.querySelector('.sideBar');
+
+    sideBar.style.display = 'none';
+     
+}
